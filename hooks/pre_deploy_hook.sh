b18964dbd64e5ccd2315c12de52dc61affec96a1
Xavi <xavi_tb@hotmail.com>
2020-04-25 17:18:41 +0200

    ci commit

--- hooks/pre_deploy_hook.sh
+++ hooks/pre_deploy_hook.sh
@@ -11,7 +11,34 @@
 # The script does the following:
 #  - Run "npm install" in each sourceDir in skill.json
 
+SKILL_NAME=$1
+DO_DEBUG=${2:-false}
+TARGET=${3:-"all"}
 
+if [ $DO_DEBUG == false ]
+then
+    exec > /dev/null 2>&1
+fi
+
+install_dependencies() {
+    npm install --prefix "$1" >/dev/null 2>&1 
+    return $?
+}
+
+echo "###########################"
+echo "##### pre-deploy hook #####"
+echo "###########################"
+
+if [[ $TARGET == "all" || $TARGET == "lambda" ]]; then
+    grep "sourceDir" ./skill.json | cut -d: -f2 |  sed 's/"//g' | sed 's/,//g' | while read -r SOURCE_DIR; do
+        if install_dependencies $SOURCE_DIR; then
+            echo "Codebase ($SOURCE_DIR) built successfully."
+        else
+            echo "There was a problem installing dependencies for ($SOURCE_DIR)."
+            exit 1
+        fi
+    done
+    echo "###########################"
+fi
 
 exit 0
-
