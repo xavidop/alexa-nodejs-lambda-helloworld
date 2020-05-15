a3f9018c38afed21e35984f60f55a06391e0e866
Xavi <xavi_tb@hotmail.com>
2020-05-15 23:08:36 +0200

    added submit step

--- test/submit/submit.sh
+++ test/submit/submit.sh
@@ -8,6 +8,8 @@ echo "######### Submiting Skill for certification without publishing #########"
 if [[ ${cli_version} == *"v1"* ]]
 then
     submit=$(ask api submit -s ${skill_id} --publication-method MANUAL_PUBLISHING)
+    exit 0
 else
     submit=$(ask smapi submit-skill-for-certification -s ${skill_id} --publication-method MANUAL_PUBLISHING)
+    exit 0
 fi
